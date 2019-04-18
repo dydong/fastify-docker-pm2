72cbce8674bc31e01583b89f1cdda8a2f5532f9a
douyadong <douyadong@wesai.com>
2019-04-18 22:31:58 +0800

    authController 禁用邮件验证功能

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,15 +1,19 @@
-const nconf = require('nconf');
+const nconf = require("nconf");
 
-const User = require('../models/User');
-const { SignUpResponse } = require('../models/Auth');
-const { INVALID_PASSWORD, USER_DOESNT_EXISTS, USER_EXISTS } = require('../models/Errors');
-const { VERIFICATION_EMAIL_SENT } = require('../models/Messages');
-const { getRandomBytes } = require('../utils/crypto');
-const EmailVerifyToken = require('../models/EmailVerifyToken');
-const { generateEmailVerifyEmail } = require('../models/EmailTemplate');
-const { sendEmail } = require('../utils/aws-ses');
+const User = require("../models/User");
+const { SignUpResponse } = require("../models/Auth");
+const {
+    INVALID_PASSWORD,
+    USER_DOESNT_EXISTS,
+    USER_EXISTS
+} = require("../models/Errors");
+const { VERIFICATION_EMAIL_SENT } = require("../models/Messages");
+const { getRandomBytes } = require("../utils/crypto");
+const EmailVerifyToken = require("../models/EmailVerifyToken");
+const { generateEmailVerifyEmail } = require("../models/EmailTemplate");
+const { sendEmail } = require("../utils/aws-ses");
 
-const isEmailVerificationEnabled = nconf.get('app.isEmailVerificationEnabled');
+const isEmailVerificationEnabled = nconf.get("app.isEmailVerificationEnabled");
 
 const postLogin = async (req, res) => {
     const { email, password } = req.body;
@@ -21,7 +25,10 @@ const postLogin = async (req, res) => {
         const isMatch = await user.comparePassword(password);
         if (isMatch) {
             const { id } = user;
-            const token = await res.jwtSign({ id }, { expiresIn: nconf.get('app.userJwtExpiry') });
+            const token = await res.jwtSign(
+                { id },
+                { expiresIn: nconf.get("app.userJwtExpiry") }
+            );
             return res.send(new SignUpResponse({ email, token, id }));
         }
         return res.send(new Error(INVALID_PASSWORD));
@@ -41,37 +48,61 @@ const postSignup = async (req, res) => {
         }
         const user = new User({
             email,
-            password,
+            password
         });
         const newUser = await user.save();
         const { id, email: userEmail } = newUser;
-        if (isEmailVerificationEnabled) {
-            const token = getRandomBytes(16);
-            const emailVerifyToken = new EmailVerifyToken({
-              userId: id,
-              token,
-            });
-            await emailVerifyToken.save();
-            const {
-              mailContent, recipient, sender, subject,
-            } = await generateEmailVerifyEmail({ email, token, userName: 'TBD' });
-            await sendEmail({
-              from: sender,
-              to: recipient,
-              message: mailContent,
-              subject,
-            });
-            res.send(new SignUpResponse({ email: userEmail, message: VERIFICATION_EMAIL_SENT, id }));
-          } else {
-            const token = await res.jwtSign({ id }, { expiresIn: nconf.get('app.userJwtExpiry') });
-            res.send(new SignUpResponse({ email: userEmail, token, id }));
-          }
+        res.send(
+            new SignUpResponse({
+                email: userEmail,
+                message: VERIFICATION_EMAIL_SENT,
+                id
+            })
+        );
     } catch (error) {
         res.send(error);
     }
 };
 
+// 暂无aws服务，临时禁止功能
+const sendEmailVerify = async newUser => {
+    const { id, email: userEmail } = newUser;
+    if (isEmailVerificationEnabled) {
+        const token = getRandomBytes(16);
+        const emailVerifyToken = new EmailVerifyToken({
+            userId: id,
+            token
+        });
+        await emailVerifyToken.save();
+        const {
+            mailContent,
+            recipient,
+            sender,
+            subject
+        } = await generateEmailVerifyEmail({ email, token, userName: "TBD" });
+        await sendEmail({
+            from: sender,
+            to: recipient,
+            message: mailContent,
+            subject
+        });
+        res.send(
+            new SignUpResponse({
+                email: userEmail,
+                message: VERIFICATION_EMAIL_SENT,
+                id
+            })
+        );
+    } else {
+        const token = await res.jwtSign(
+            { id },
+            { expiresIn: nconf.get("app.userJwtExpiry") }
+        );
+        res.send(new SignUpResponse({ email: userEmail, token, id }));
+    }
+};
+
 module.exports = {
     postLogin,
-    postSignup,
+    postSignup
 };
